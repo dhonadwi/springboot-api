40a047a0eef1993a75d716ee95a19a52f670a320
dhonadwi <dhonadwi2@gmail.com>
2023-08-15 14:13:42 +0700

    fixing ProductSerice

--- src/main/java/com/belajar/controllers/ProductController.java
+++ src/main/java/com/belajar/controllers/ProductController.java
@@ -1,8 +1,12 @@
 package com.belajar.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +27,22 @@ public class ProductController {
   }
 
   @GetMapping
-  public Iterable<Product> findAll() {
+  public ResponseEntity<Object> findAll() {
     return productService.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public  ResponseEntity<Object> findOne(@PathVariable("id") Long id) {
+    return productService.findOne(id);
+  }
+
+   @PutMapping
+   public Product update(@RequestBody Product product) {
+     return productService.save(product);
+   }
+  
+   @DeleteMapping("/{id}")
+  public void removeOne(@PathVariable("id") Long id) {
+    productService.removeOne(id);
   }
 }
--- src/main/java/com/belajar/services/ProductService.java
+++ src/main/java/com/belajar/services/ProductService.java
@@ -1,10 +1,13 @@
 package com.belajar.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.belajar.models.entities.Product;
@@ -13,7 +16,7 @@ import com.belajar.models.repos.ProductRepo;
 @Service
 @Transactional
 public class ProductService {
-  
+
   @Autowired
   private ProductRepo productRepo;
 
@@ -21,12 +24,55 @@ public class ProductService {
     return productRepo.save(product);
   }
 
-  public Product findOne(Long id) {
-    return productRepo.findById(id).get();
+  public class ResponseMessage {
+    private String message;
+    private boolean status;
+    private Object product;
+
+    public ResponseMessage(String message, boolean status, Object product) {
+      this.message = message;
+      this.status = status;
+      this.product = product;
+    }
+
+    public Object getProduct() {
+      return product;
+    }
+
+    public void setProduct(Object product) {
+      this.product = product;
+    }
+
+    public String getMessage() {
+      return message;
+    }
+
+    public void setMessage(String message) {
+      this.message = message;
+    }
+
+    public boolean isStatus() {
+      return status;
+    }
+
+    public void setStatus(boolean status) {
+      this.status = status;
+    }
   }
 
-  public Iterable<Product> findAll() {
-    return productRepo.findAll();
+  public ResponseEntity<Object> findOne(Long id) {
+    Optional<Product> product = productRepo.findById(id);
+    if (!product.isPresent()) {
+      ResponseMessage errorMessage = new ResponseMessage("data not found", false, product);
+      return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+    }
+    ResponseMessage resMessage = new ResponseMessage("data found", true, product.get());
+    return new ResponseEntity<>(resMessage, HttpStatus.OK);
+  }
+
+  public ResponseEntity<Object> findAll() {
+    ResponseMessage responseMessage = new ResponseMessage("data found", true, productRepo.findAll());
+    return new ResponseEntity<>(responseMessage, HttpStatus.OK);
   }
 
   public void removeOne(Long id) {
